a0bffc8bdef9f97f9e698604d6d47df5a5e3cefa
stupidsavacan <tetsushitakegami1124@gmail.com>
2026-06-03 00:47:33 +0900

    Fix Android signing config syntax

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         create("release") {
             if (keystorePropertiesFile.exists()) {
                 storeFile = file(keystoreProperties["storeFile"] as String)
-                storePassword = keystoreProperties["storePassword"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
                 keyAlias = keystoreProperties["keyAlias"] as String
                 keyPassword = keystoreProperties["keyPassword"] as String
             }
