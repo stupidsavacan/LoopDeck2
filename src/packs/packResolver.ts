0d7b2e57e6523fc74d30e34acb0fca19b128b66d
stupidsavacan <tetsushitakegami1124@gmail.com>
2026-06-10 20:10:06 +0900

    Track active module and question pack ownership

--- src/packs/packResolver.ts
+++ src/packs/packResolver.ts
@@ -7,6 +7,8 @@ export interface ResolvedPackView {
   packById: ReadonlyMap<string, LoopDeckPack>;
   moduleById: ReadonlyMap<string, ModuleInfo>;
   questionById: ReadonlyMap<string, Question>;
+  modulePackIdById: ReadonlyMap<string, string>;
+  questionPackIdById: ReadonlyMap<string, string>;
 }
 
 function isQuestion(question: Question | undefined): question is Question {
@@ -34,24 +36,31 @@ export function resolveActivePacks(packs: LoopDeckPack[]): ResolvedPackView {
   const activePacks = Array.from(packById.values());
 
   const moduleById = new Map<string, ModuleInfo>();
+  const modulePackIdById = new Map<string, string>();
   const availableQuestionById = new Map<string, Question>();
+  const availableQuestionPackIdById = new Map<string, string>();
 
   for (const pack of activePacks) {
     for (const module of pack.modules) {
       moduleById.set(module.id, module);
+      modulePackIdById.set(module.id, pack.packId);
     }
     for (const question of pack.questions) {
       availableQuestionById.set(question.id, question);
+      availableQuestionPackIdById.set(question.id, pack.packId);
     }
   }
 
   const activeModules = Array.from(moduleById.values());
   const activeQuestionById = new Map<string, Question>();
+  const questionPackIdById = new Map<string, string>();
 
   for (const module of activeModules) {
     for (const questionId of module.questionIds) {
       const question = availableQuestionById.get(questionId);
+      const packId = availableQuestionPackIdById.get(questionId);
       if (question) activeQuestionById.set(question.id, question);
+      if (question && packId) questionPackIdById.set(question.id, packId);
     }
   }
 
@@ -61,7 +70,9 @@ export function resolveActivePacks(packs: LoopDeckPack[]): ResolvedPackView {
     questions: Array.from(activeQuestionById.values()),
     packById,
     moduleById,
-    questionById: activeQuestionById
+    questionById: activeQuestionById,
+    modulePackIdById,
+    questionPackIdById
   };
 }
 
@@ -83,6 +94,10 @@ export function getModuleById(view: ResolvedPackView, moduleId: string): ModuleI
 
 export function getQuestionById(view: ResolvedPackView, questionId: string): Question | undefined {
   return view.questionById.get(questionId);
+}
+
+export function getQuestionPackId(view: ResolvedPackView, questionId: string): string | undefined {
+  return view.questionPackIdById.get(questionId);
 }
 
 export function getQuestionsForModule(view: ResolvedPackView, moduleOrId: ModuleInfo | string | undefined): Question[] {
